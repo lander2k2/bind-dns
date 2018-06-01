cfa4fd40b7594a7f9a2f02d460fc63c8cf4d5824
Richard Lander <lander2k2@protonmail.com>
2018-06-01 17:19:57 -0400

    Clear any iptables rules

--- images/install_bind_centos.sh
+++ images/install_bind_centos.sh
@@ -5,3 +5,12 @@ sudo yum update -y
 sudo yum clean all
 sudo sudo yum install -y bind bindutils
 
+# reset any existing iptables rules
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X
+
